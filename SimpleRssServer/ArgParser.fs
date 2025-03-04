79515bdc7b83c29a4d37e3e2b680889fb4a5e60d
Roald Ruiter <roaldruiter@gmail.com>
2025-03-04 21:09:08 +0100

    feat: Add log level option to ParsedArgs in ArgParser module

--- SimpleRssServer/ArgParser.fs
+++ SimpleRssServer/ArgParser.fs
@@ -1,9 +1,14 @@
 module ArgParser
 
-type ParsedArgs = { Hostname: string option }
+open Microsoft.Extensions.Logging
+
+type ParsedArgs = { 
+    Hostname: string option
+    Loglevel: LogLevel option
+    }
 
 let parse (args: string) : ParsedArgs =
     let parts = args.Split(' ')
     match parts with
-    | [| "--hostname"; hostname |] -> { Hostname = Some hostname }
-    | _ -> { Hostname = None }
+    | [| "--hostname"; hostname |] -> { Hostname = Some hostname; Loglevel = None }
+    | _ -> { Hostname = None; Loglevel = None }
