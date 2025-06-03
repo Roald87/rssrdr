9546fc98211aa3d347c0d55a20ea831f8ee667c6
Roald Ruiter <roaldruiter@gmail.com>
2025-06-03 22:05:06 +0200

    remove unused files

--- SimpleRssServer/CacheModule.fs
+++
@@ -1,8 +0,0 @@
-module SimpleRssServer.CacheModule
-
-open System
-
-// Placeholder content for CacheModule
-let isCacheOld (cacheDate: DateTime) =
-    let currentDate = DateTime.Now
-    (currentDate - cacheDate).TotalDays > 7.0
