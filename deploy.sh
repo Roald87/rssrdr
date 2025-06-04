4dccefc0a5c8020e7e8a6d6a18c3bc5123a4f326
Roald Ruiter <roaldruiter@gmail.com>
2025-06-04 22:02:37 +0200

    add tag to deploy script

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,11 @@ read -p "Enter new version number: " new_version
 sed -i "s/<Version>.*<\/Version>/<Version>$new_version<\/Version>/" SimpleRssServer/SimpleRssServer.fsproj
 git add SimpleRssServer/SimpleRssServer.fsproj
 git commit -m "bump version number"
+git tag -a "$new_version" -m "Version $new_version"
 dotnet publish
 sudo systemctl stop rssrdr-server.service
 find /var/www/rssrdr/* -not -name 'rss-cache' -not -path '/var/www/rssrdr/rss-cache/*' -exec rm -rf {} +
 cp -r SimpleRssServer/bin/Release/net8.0/publish/* /var/www/rssrdr/
 sudo systemctl start rssrdr-server.service
+git push
+git push origin "$new_version"
