6174ffdae61788cb7f75cc77662a9a79decc46bb
Roald Ruiter <roaldruiter@gmail.com>
2025-06-15 17:21:00 +0200

    make net version into variable and print current version number

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 git pull
+current_version=$(grep -oP '(?<=<Version>).*?(?=</Version>)' SimpleRssServer/SimpleRssServer.fsproj)
+echo "Current version: $current_version"
 read -p "Enter new version number: " new_version
 sed -i "s/<Version>.*<\/Version>/<Version>$new_version<\/Version>/" SimpleRssServer/SimpleRssServer.fsproj
 git add SimpleRssServer/SimpleRssServer.fsproj
 git commit -m "bump version number"
 git tag $new_version
+dotnet_version=$(grep -oP '(?<=<TargetFramework>).*?(?=</TargetFramework>)' SimpleRssServer/SimpleRssServer.fsproj)
 dotnet publish
 sudo systemctl stop rssrdr-server.service
 find /var/www/rssrdr/* -not -name 'rss-cache' -not -path '/var/www/rssrdr/rss-cache/*' -exec rm -rf {} +
-cp -r SimpleRssServer/bin/Release/net8.0/publish/* /var/www/rssrdr/
+cp -r SimpleRssServer/bin/Release/$dotnet_version/publish/* /var/www/rssrdr/
 sudo systemctl start rssrdr-server.service
 git push origin "$new_version"
