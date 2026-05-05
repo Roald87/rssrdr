d89d9c3b11ae39349407f2e629fda66a08c21c9d
Roald87 <roaldruiter@gmail.com>
2026-05-05 16:54:48 +0200

    limit maximum degree of parallelism (#96)

--- SimpleRssServer/Request.fs
+++ SimpleRssServer/Request.fs
@@ -62,7 +62,7 @@ let fetchAllRssFeeds client logger (cacheConfig: CacheConfig) (ups: UriProcessSt
             |> List.map (function
                 | PendingFetch(dt, uri) -> fetchUri client logger cacheConfig (dt, uri)
                 | x -> async.Return x)
-            |> Async.Parallel
+            |> fun asyncs -> Async.Parallel(asyncs, maxDegreeOfParallelism = 8)
 
         return List.ofArray processed
     }
